5d6742fc03cb60006cc92c2a4ce7f1401125b9e9
B.ziva8702505 <zivarab@code.edu.az>
2022-04-12 06:15:59 +0400

    task

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Consolehomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Username daxil edin:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Password daxil edin:");
+            string password = Console.ReadLine();
+
+            User user = new User(userName, password);
+            user.UserName = "hhehcjvjk";
+            user.Password = "5cvqregt";
+        }
+
+    }
+}
+
+
+
---
+++ User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Consolehomework
+{
+    class User
+    {
+
+        public string _userName;
+        public string _password;
+        int minLength = 8;
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (value.Length >= minLength)
+                {
+                    _userName = value;
+                    return;
+                }
+                Console.WriteLine("Yeniden Username daxil edin");
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Length >= minLength)
+                {
+                    _password = value;
+                    return;
+                }
+                Console.WriteLine("Passwordu yeniden daxil edin");
+                if (!Password.Any(char.IsDigit))
+                {
+                    return;
+                }
+                if (!Password.Any(char.IsUpper))
+                {
+                    return;
+                }
+                if (!Password.Any(char.IsLower))
+                {
+                    return;
+                }
+            }
+        }
+
+        public User(string username, string password)
+        {
+            username = UserName;
+            password = Password;
+        }
+    }
+}
+     
